943cc4228464f412a7971dcec72b3463cdc4bee4
bart0040 <bart0040@hz.nl>
2021-04-20 10:43:57 +0200

    added the 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+<div class="text" style="background-color:white">
+    <p>This is an 404 error page, this means we have lost control of what you are doing so we could not return your
+        request</p>
+    <p>Please return to the main page using this <a href="/">Link</a></p>
+</div>
